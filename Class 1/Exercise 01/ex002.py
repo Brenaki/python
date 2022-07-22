5bf398efb1c15cb974d2de0e1024bce1f4e0d673
Victor Angelo Legat Cerqueira <91611155+Brenaki@users.noreply.github.com>
2022-07-22 16:35:16 -0300

    Exercises

---
+++ Class 1/Exercise 01/ex002.py
@@ -0,0 +1,3 @@
+name = input ('What\'s your name?\n')
+
+print('Nice to meet you, {}!'.format(name))
--- Class 1/Hello World 00/Challenge 01.py
+++ Class 1/Hello World 00/Challenge 01.py
@@ -1,3 +1,4 @@
 print('===== Challenge 01 =====')
 name = input ('What\'s your name?\n')
 print('Hello', name+'! \nNice to meet you!')
+
