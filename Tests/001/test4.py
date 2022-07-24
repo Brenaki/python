bdb4841b7c2e9549784a7e093e159c4a1464f0e7
Victor Angelo Legat Cerqueira <91611155+Brenaki@users.noreply.github.com>
2022-07-23 23:04:57 -0300

    Update 005

---
+++ Tests/001/test4.py
@@ -0,0 +1,14 @@
+# Challenge - Make a program that asks for two numbers and prints the largest of them!
+
+# Title
+print('=====Decision Structure - Challenges=====')
+
+# Numbers
+n1 = int(input ('Write fist number: '))
+n2 = int(input ('Write second number: '))
+
+# Check
+if(n1 > n2):
+    print('The first number is greater than the second number. {} > {}'.format(n1, n2))
+else:
+    print('The second number is greater than the fist number. {} > {}'.format(n2, n1))
