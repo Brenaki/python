3b717363d0f10f38e9a229f8e3a9286fe99c30c2
Victor Angelo Legat Cerqueira <91611155+Brenaki@users.noreply.github.com>
2022-08-09 12:14:10 -0300

    Update 007
    *Só adicionado alguns arquivos do meu curso.
    
    **Adding some files from my course.

---
+++ Curso_Python/MÓDULO 06 - Website with Python/main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Criar a 1º página do site
+# route -> cergat.com/
+# funtion -> o que você quer exibir naquela página
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/contatos')
+def contatos():
+    return render_template('contatos.html')
+
+@app.route('/usuarios/<nome_usuario>')
+def usuarios(nome_usuario):
+    return render_template('usuarios.html', nome_usuario=nome_usuario)
+
+# Colocar o site no ar
+if __name__ == '__main__':
+    app.run(debug = True)
+
+    # servidor do heroku
