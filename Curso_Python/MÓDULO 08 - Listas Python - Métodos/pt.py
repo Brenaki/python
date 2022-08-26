a55ba4343749e7eda5bfc966ba1e12440b0c96f2
Victor Angelo Legat Cerqueira <91611155+Brenaki@users.noreply.github.com>
2022-08-26 09:39:18 -0300

    Wallpaper

---
+++ Curso_Python/MÓDULO 08 - Listas Python - Métodos/pt.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+food = 'code'
+sleep = 'computer'
+life = 'program'
+
+print('I eat {}, sleep on the {} and live to {}'.format(food, sleep, life))
+
+'I eat code, sleep on the computer and live to the program'
