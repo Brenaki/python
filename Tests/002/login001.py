a82d9cf69678469f7402d98bdb29430d18a5496c
Victor Angelo Legat Cerqueira <91611155+Brenaki@users.noreply.github.com>
2022-09-02 10:16:14 -0300

    Test 003
    Consegui!!!!!

--- Tests/002/login001.py
+++ Tests/002/login001.py
@@ -1,21 +1,30 @@
 # Fazer uma sistema de login, porém é como se o usuario
 # estivesse errando a senha, então pode redefinir a senha
 # e poderá logar
+from ast import Break
+
+
+new_P = '1234'
 
 while True:
     user = input('Your user: ')
     password = input('Your password: ')
-    if password != '':
-        if password != '1234' or password != new_P:
+    if password != '': 
+        if password != new_P and not '1234' in password :
+            print('Senha inválida')
             r_pass = input('Quer redifinir sua senha(N/Y): ')
             if r_pass == 'N' or r_pass == 'n':
                continue
             else:
                 new_P = input('New password: ')
-                password = new_P
+                while password != new_P:
+                      user = input('Your user: ')
+                      password = input('Your password: ')
+                print('Sua nova senha foi redifinida!')
                 continue
-        else:
-          break
+        elif password == '1234' or password == new_P:
+            print('Logado!')
+            break
     else:
       break
 
