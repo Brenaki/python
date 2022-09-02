bbfec43e255b7576379f4ba6d6926f4979cc29cc
Victor Angelo Legat Cerqueira <91611155+Brenaki@users.noreply.github.com>
2022-09-02 09:24:42 -0300

    Test 002

--- Tests/002/login001.py
+++ Tests/002/login001.py
@@ -2,19 +2,21 @@
 # estivesse errando a senha, então pode redefinir a senha
 # e poderá logar
 
-user = input('Your user: ')
-password = input('Your password: ')
-
 while True:
     user = input('Your user: ')
     password = input('Your password: ')
     if password != '':
-        while password != '1234':
+        if password != '1234' or password != new_P:
             r_pass = input('Quer redifinir sua senha(N/Y): ')
             if r_pass == 'N' or r_pass == 'n':
-               password = input('Your password: ')
+               continue
             else:
-                new_password = input('New password: ')
-                password = new_password
+                new_P = input('New password: ')
+                password = new_P
+                continue
+        else:
+          break
+    else:
+      break
 
 
